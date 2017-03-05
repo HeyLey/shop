c2a2ae428719ffa558890be55cf3abb29ee8a2c8
HeyLey <leila.xr@gmail.com>
2017-03-06 02:22:54 +0300

    Add new products

--- resources/org/leyla/templates/template.ftl
+++ resources/org/leyla/templates/template.ftl
@@ -60,17 +60,19 @@
         <ul class="sub-menu">
             <li><a href="#1">Платья</a></li>
             <li><a href="#2">Юбки</a></li>
-            <li><a href="#3">Блузки</a></li>
-            <li><a href="#4">Верхняя одежда</a></li>
-            <li><a href="#5">Обувь</a></li>
+            <li><a href="#4">Брюки</a></li>
+            <li><a href="#5">Блузки</a></li>
+            <li><a href="#6">Верхняя одежда</a></li>
+            <li><a href="#7">Обувь</a></li>
         </ul>
         <li><a href="#man">Для мужчин</a></li>
         <ul class="sub-menu">
-            <li><a href="#1">Костюмы</a></li>
+            <li><a href="#1">Пиджаки</a></li>
             <li><a href="#2">Брюки</a></li>
             <li><a href="#3">Рубашки</a></li>
-            <li><a href="#4">Верхняя одежда</a></li>
-            <li><a href="#5">Обувь</a></li>
+            <li><a href="#4">Футболки</a></li>
+            <li><a href="#5">Верхняя одежда</a></li>
+            <li><a href="#6">Обувь</a></li>
         </ul>
         <li><a href="/products_page.jsp">Все товары</a></li>
     </ul>
--- src/org/leyla/ProductsProvider.java
+++ src/org/leyla/ProductsProvider.java
@@ -27,7 +27,30 @@ public class ProductsProvider {
                 new Product(12, "Блуза Sportmax Code", "23 999 руб.", 23999, "/img/img3.png", "/img/img3.jpg", "Черный", "Красный", "Синий"),
                 new Product(13, "Юбка Twin-Set Simona Barbieri", "25 599 руб.",25599, "/img/img4.png", "/img/img4.jpg", "Серый", "Черный", "Бежевый"),
                 new Product(14, "Юбка Elisabetta Franchi", "15 999 руб.", 15999,  "/img/img5.png", "/img/img5.jpg", "Красный", "Черный", "Синий"),
-                new Product(15, "Юбка Elmira Markes", "14 100 руб.", 14100, "/img/img6.png", "/img/img6.jpg", "Черный", "Серебристый", "Золотой")
+                new Product(15, "Юбка Elmira Markes", "14 100 руб.", 14100, "/img/img6.png", "/img/img6.jpg", "Черный", "Серебристый", "Золотой"),
+                new Product(31, "Брюки Boutique Moschino", "28 499 руб.", 28499, "/img/img31.png", "/img/img31.jpg", "Желтый", "Розовый", "Голубой"),
+                new Product(32, "Брюки French Connection", "26 999 руб.", 26999,  "/img/img32.png", "/img/img32.jpg", "Коричневый", "Красный", "Черный"),
+                new Product(33, "Брюки Max&Co", "19 999 руб.", 19999, "/img/img33.png", "/img/img33.jpg", "Бордовый", "Черный", "Синий"),
+
+
+                new Product(16, "Рубашка Just Cavalli", "19 999 руб.", 19999, "/img/img7.png", "/img/img7.jpg", "Черный", "Синий", "Серый"),
+                new Product(17, "Рубашка Love Moschino", "14 999 руб.", 14999, "/img/img8.png","/img/img8.jpg", "Белый", "Черный", "Серый"),
+                new Product(18, "Рубашка NIGHT RIDE Affliction", "11 499 руб.", 11499, "/img/img9.png", "/img/img9.jpg", "Красный", "Серый", "Черный"),
+                new Product(19, "Пуховик Woolrich", "82 199 руб.",82199, "/img/img13.png", "/img/img13.jpg", "Черный", "Зеленый", "Коричневый"),
+                new Product(20, "Дубленка Mondial", "69 900 руб.", 69900,  "/img/img14.png", "/img/img14.jpg", "Черный", "Коричневый", "Бежевый"),
+                new Product(21, "Пуховик C.P. Company", "53 899 руб.", 53899 , "/img/img15.png", "/img/img15.jpg", "Красный", "Синий", "Черный"),
+                new Product(22, "Брюки спортивные Markus Lupfer", "27 399 руб.", 27399,  "/img/img19.png", "/img/img19.jpg", "Черно-серый", "Черно-синий", "Черно-белый"),
+                new Product(23, "Брюки Boss", "21 000 руб.", 21000,  "/img/img20.png", "/img/img20.jpg", "Черный", "Серый", "Белый"),
+                new Product(24, "Чиносы Iceberg", "16 299 руб.", 16299 ,  "/img/img21.png", "/img/img21.jpg", "Белый", "Серый", "Синий"),
+                new Product(25, "Пиджак Boss", "48 000 руб.", 48000, "/img/img25.png", "/img/img25.jpg", "Черный", "Серый", "Белый"),
+                new Product(26, "Пиджак Joop!", "35 999 руб.", 35999, "/img/img26.png","/img/img26.jpg", "Серый", "Белый", "Черный"),
+                new Product(27, "Пиджак Tommy Hilfiger", "33 990 руб.", 33990, "/img/img27.png", "/img/img27.jpg", "Бежевый", "Черный", "Серый"),
+                new Product(28, "Лоферы Roberto Cavalli", "39 999 руб.",39999, "/img/img28.png", "/img/img28.jpg", "Черный", "Серый", "Коричневый"),
+                new Product(29, "Туфли Roberto Botticelli", "49 399 руб.", 49399,  "/img/img29.png", "/img/img29.jpg", "Черный", "Коричневый", "Бежевый"),
+                new Product(30, "Туфли Moma", "29 999 руб.", 29999, "/img/img30.png", "/img/img30.jpg", "Синий", "Черный", "Белый"),
+                new Product(34, "Футболка Plein Sport", "24 499 руб.",24499, "/img/img34.png", "/img/img34.jpg", "Белый", "Черный", "Красный"),
+                new Product(35, "Поло Love Moschino", "18 999 руб.", 18999,  "/img/img35.png", "/img/img35.jpg", "Красный", "Синий", "Зеленый"),
+                new Product(36, "Футболка Versace Jeans", "14 499 руб.", 14499, "/img/img36.png", "/img/img36.jpg", "Зеленый", "Желтый", "Синий")
 
         );
     }
